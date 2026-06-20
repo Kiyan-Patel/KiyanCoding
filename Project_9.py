6069d2d2f5c144e165af38f9374331805faef946
Kiyan-Patel <patel.kiyan0505@gmail.com>
2026-06-20 14:33:57 -0400

    Project9

---
+++ Project_9.py
@@ -0,0 +1,8 @@
+a=int(input("Enter your age"))
+if a>=10:
+    if a<=20:
+        print("Your age is between 10 and 20")
+    else:
+        print("Your age is not between 10 and 20")
+else:
+    print("Your age is not above 10")
